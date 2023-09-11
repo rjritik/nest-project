72707ab2616dfbdb4b677580ed78a47b4dab60a7
ritik <rjritik123@gmail.com>
2023-09-11 12:22:04 +0530

    first commit

---
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class EditBookmarkDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  link?: string;
+}
